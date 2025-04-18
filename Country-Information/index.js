5c1a608d641718f43bd15f2cef4268f870586613
Githieya Kevin <githieyakevin@gmail.com>
2025-04-18 13:13:17 +0300

    Responsivity

--- Country-Information/index.js
+++ Country-Information/index.js
@@ -4,13 +4,18 @@ document.getElementById("button").addEventListener("click", ()=>{
   fetch(`https://restcountries.com/v3.1/name/${input.value}`)
     .then(request => request.json())
     .then(data=>{
-      let flag = document.createElement("img")
+      console.log(data)
+
+
+      const flag = document.createElement("img")
       flag.setAttribute("src", data[0].flags.png)
       flag.setAttribute("alt", "Country flag")
       flag.setAttribute("class", "flag")
       document.getElementById("countryContainer").prepend(flag) ;
 
-      let court = document.createElement("img")
+
+
+      const court = document.createElement("img")
       court.setAttribute("src", data[0].coatOfArms.png)
       court.setAttribute("alt", "Court of Arms")
       court.setAttribute("class", "Court")
@@ -21,6 +26,7 @@ document.getElementById("button").addEventListener("click", ()=>{
      document.getElementById("capitalCity").textContent = "CAPITAL CITY: "+data[0].capital[0]
      document.getElementById("continent").textContent = "CONTINENT: "+data[0].continents[0]
      document.getElementById("population").textContent = "POPULATION: "+data[0].population + " People"
+
    }).catch(error=>{
      alert("Error has occured!!!, error message: " + error.message)
    })
